fd15e16fd7b561707035af82d519df5f60c084dd
mspencersmith <mspencersmith@outlook.com>
2020-12-11 16:45:14 +0000

    Created neural net and data maker

---
+++ make_data_set.py
@@ -0,0 +1,49 @@
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+
+class Data():
+    img_wid = 128
+    img_hei = 72
+    ran = "bcf_images/random"
+    bc = "bcf_images/badminton_court"
+    LABELS = {ran: 0, bc: 1}
+    data_set = []
+    
+    ran_count = 0
+    bc_count = 0
+
+    def make_data_set(self):
+        for label in self.LABELS:
+            print(label)
+            for f in tqdm(os.listdir(label)):
+                if ".jpg" in f:
+                    try:
+                        path = os.path.join(label, f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (self.img_wid, self.img_hei))
+                        self.data_set.append([np.array(img), np.eye(4)
+                            [self.LABELS[label]]])
+                        # print(np.eye(2)[self.LABELS[label]])
+                        # print(self.data_set[0])
+
+                        if label == self.ran:
+                            self.ran_count += 1
+                        elif label == self.bc:
+                            self.bc_count += 1
+
+                    except Exception as e:
+                        pass
+                        #print(label. f, str(e))
+
+        np.random.shuffle(self.data_set)
+        np.save("bc_data/data_set.npy", self.data_set)
+        print("Random: ", self.ran_count)
+        print("Badminton Court: ", self.bc_count)
+        print(f"Total: {len(self.data_set)}")
+
+
+data = Data()
+data.make_data_set()
---
+++ nn_model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5) # 1 image, 32 outputs, 5x5 kernel
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        # pass through random data to flatten for linear
+        x = torch.randn(128, 72).view(-1, 1, 128, 72)
+        self._to_linear = None
+        self.convs(x)
+
+
+        self.fc1 = nn.Linear(self._to_linear, 512) # flattening
+        self.fc2 = nn.Linear(512, 2) # 2 classes
+
+    def convs(self, x):
+        # max pooling over 2x2
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
+
+        #Flatten
+        # print(x[0].shape)
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear) # .view is reshape, flattens x before
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+net = Net()
+print(net)
