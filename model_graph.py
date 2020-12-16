7c07ac6850f3f07f4fc6c2bbb5747b3bd513d53b
mspencersmith <mspencersmith@outlook.com>
2020-12-16 16:27:19 +0000

    Trained model to 97% accuracy on validation set

--- model_graph.py
+++ model_graph.py
@@ -3,7 +3,7 @@ from matplotlib import style
 
 style.use("ggplot")
 
-model_name = "model-1608042373-lr5e-06-factor0.1pat10-thr1e-06-val_pct0.2-batches150-128-72-128"
+model_name = "model-1608135008-lr1e-06-factor0.1pat2-thr0.01-val_pct0.2-bat_size150-128-72-512"
 
 
 def create_acc_loss_graph(model_name):
@@ -38,13 +38,13 @@ def create_acc_loss_graph(model_name):
     ax1.plot(times, accuracies, label="acc")
     ax1.plot(times, val_accuracies, label="val_acc")
     ax1.legend(loc=2)
-    ax1.axis([times[0], times[-1], -1, 1])
+    ax1.axis([times[0], times[-1], 0, 1])
     # ax1.axis([times[0], times[-1], -100, 100])
     ax2.plot(times, losses, label="loss")
     ax2.plot(times, val_losses, label="val_loss")
     ax2.legend(loc=2)
-    # ax2.axis([times[0], times[-1], 0, 0.003])
-    ax2.axis([times[0], times[-1], 0, 2])
+    ax2.axis([times[0], times[-1], 0, 0.05])
+    # ax2.axis([times[0], times[-1], 0, 20])
     plt.show()
     print(len(times))
 
--- train_and_test.py
+++ train_and_test.py
@@ -20,22 +20,22 @@ print("\nCreating neural network.. \n")
 net = Net().to(device)
 
 start = time.time()
-lr = 0.000005
+lr = 0.000001
 fact = 0.1
-pat = 10
-thr = 0.000001
+pat = 2
+thr = 0.01
 val_pct = 0.20 # reserves % of data for validation
-batch_size = 150
-lin_lay = 128
+bat_size = 150
+lin_lay = 512
 
 
-model_name = f"model-{int(start)}-lr{lr}-factor{fact}pat{pat}-thr{thr}-val_pct{val_pct}-batches{batch_size}-{img_wid}-{img_hei}-{lin_lay}"
+model_name = f"model-{int(start)}-lr{lr}-factor{fact}pat{pat}-thr{thr}-val_pct{val_pct}-bat_size{bat_size}-{img_wid}-{img_hei}-{lin_lay}"
 file = f"bcf_models/{model_name}.pth"
 
 
 data_set = np.load("bcf_data/data_set.npy", allow_pickle=True)
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-# scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=fact, patience=pat, threshold=thr, verbose=True)
+optimizer = optim.Adam(net.parameters(), lr=lr)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=fact, patience=pat, threshold=thr, verbose=True)
 loss_function = nn.MSELoss()
 
 X = torch.Tensor([i[0] for i in data_set]).view(-1, 128, 72)
@@ -58,23 +58,20 @@ def fwd_pass(X, y, train=False):
 
     return acc, loss
 
-def _prep_batch(X, y, batches, test=True):
-    if test:
-        val_acc, val_loss = 0, 0
-    for i in tqdm(range(0, len(X), batches)):
-        batch_X = X[i:i+batches].view(-1, 1, img_wid, img_hei)
-        batch_y = y[i:i+batches]
+def _prep_batch(X, y, batch_size, train=False):
+    acc_count, loss_count = 0, 0
+    length = len(X)
+    batches = length / batch_size
+    for i in tqdm(range(0, length, batch_size)):
+        batch_X = X[i:i+batch_size].view(-1, 1, img_wid, img_hei)
+        batch_y = y[i:i+batch_size]
         batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-        if test:
-            accuracy, loss = fwd_pass(batch_X, batch_y)
-            val_acc += accuracy / 100
-            val_loss += loss
-        else:
-            accuracy, loss = fwd_pass(batch_X, batch_y, train=True)
-    if test:
-        accuracy = val_acc / batches
-        loss = val_loss / batches
-    return accuracy, loss
+        accuracy, loss = fwd_pass(batch_X, batch_y, train)
+        acc_count += accuracy
+        loss_count += loss
+    acc_count = acc_count / batches
+    loss_count = loss_count / batches
+    return acc_count, loss_count
 
 def test():
     test_batch = 10
@@ -87,20 +84,17 @@ def test():
     return val_acc, val_loss
 
 def train(net, save=False):
-    EPOCHS = 5
+    EPOCHS = 50
     train_X = X[:-val_size]
     train_y = y[:-val_size]
-    print(f"Training {model_name}, batch_size: {batch_size}, EPOCHS: {EPOCHS}")
+    print(f"Training {model_name}, batch_size: {bat_size}, EPOCHS: {EPOCHS}")
     start_train = time.time()
     with open(f"bcf_logs/{model_name}.log", "a") as f:
         for epoch in range(EPOCHS):
             print(epoch)
-            acc, loss = _prep_batch(train_X, train_y, batch_size, test=False)
- 
-            # acc = acc / batch_size
-            # loss = loss / batch_size
+            acc, loss = _prep_batch(train_X, train_y, bat_size, train=True)
             val_acc, val_loss = test()
-            # scheduler.step(val_loss)
+            scheduler.step(val_loss)
             f.write(
                     f"{model_name},{round(time.time(),3)},{round(float(acc),4)},{round(float(loss),8)},{round(float(val_acc),4)},{round(float(val_loss),8)},{epoch}\n")
     
@@ -111,59 +105,6 @@ def train(net, save=False):
         print(f"\n{model_name} took {duration} minutes to train and was saved in {file}")
     else:
         print(f"\n{model_name} took {duration} minutes to train.")
-# def test():
-#     test_batch = 10
-#     test_X, test_y = X[-val_size:], y[-val_size:]
-#     val_acc, val_loss = 0, 0
-#     for i in tqdm(range(0, len(test_X), test_batch)):
-#         with torch.no_grad():
-#             batch_X = test_X[i:i+test_batch].view(-1, 1, img_wid, img_hei)
-#             batch_y = test_y[i:i+test_batch]
-#             batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-#             bat_acc, bat_loss = fwd_pass(batch_X, batch_y)
-#             val_acc += bat_acc
-#             val_loss += bat_loss
-#     val_acc = val_acc / test_batch
-#     val_loss = val_loss / test_batch
-#     print("\nOut of sample test")
-#     print(f"val_acc: {val_acc}, val_loss: {val_loss}")
-#     return val_acc, val_loss
-
-# def train(net, save=False):
-#     EPOCHS = 10
-#     train_X = X[:-val_size]
-#     train_y = y[:-val_size]
-#     print(f"Training {model_name}, batch_size: {batch_size}, EPOCHS: {EPOCHS}")
-#     start_train = time.time()
-#     with open(f"bcf_logs/{model_name}.log", "a") as f:
-#         for epoch in range(EPOCHS):
-#             print(epoch)
-#             # acc, loss = 0, 0
-#             for i in tqdm(range(0, len(train_X), batch_size)):
-#                 batch_X = train_X[i:i+batch_size].view(-1, 1, img_wid, img_hei)
-#                 batch_y = train_y[i:i+batch_size]
-
-#                 batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-
-#                 # bat_acc, bat_loss = fwd_pass(batch_X, batch_y, train=True)
-#                 acc, loss = fwd_pass(batch_X, batch_y, train=True)
-#                 # acc += bat_acc
-#                 # loss += bat_loss
- 
-#             # acc = acc / batch_size
-#             # loss = loss / batch_size
-#                 val_acc, val_loss = test()
-#                 # scheduler.step(val_loss)
-#                 f.write(
-#                     f"{model_name},{round(time.time(),3)},{round(float(acc),2)},{round(float(loss),4)},{round(float(val_acc),2)},{round(float(val_loss),4)},{epoch}\n")
-    
-#     finish_train = time.time()
-#     duration = round((finish_train - start_train)/60, 2)
-#     if save == True:
-#         torch.save(net.state_dict(), file)
-#         print(f"\n{model_name} took {duration} minutes to train and was saved in {file}")
-#     else:
-#         print(f"\n{model_name} took {duration} minutes to train.")
 
 train(net)
 # train(net, save=True)
