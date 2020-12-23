8e32c7a70976d379ce09f894b8d54f97476b12be
mspencersmith <mspencersmith@outlook.com>
2020-12-23 13:26:50 +0000

    Fixed bugs when loading model

--- inference.py
+++ inference.py
@@ -23,46 +23,46 @@ class Inference:
     def load_model(self):
         """Loads model on to GPU if availible otherwise loads to CPU"""
         
-        if "cuda" in self.processor and torch.cuda.is_available==False():
+        if "cuda" in self.processor and torch.cuda.is_available()==False:
             print("\nCuda unavailable\n")
             self.processor = "cpu"
         print(f"\nLoading neural network on {self.processor}..\n")
-        self.device, self.net = to_dev()
+        self.device, self.net = self._to_dev()
         
     def _to_dev(self):
         """Initialises network onto device"""
         
         self.device = torch.device(self.processor)
         self.net = Net().to(self.device)
-        self.net.load_state_dict(torch.load(self.model, map_location=device))
+        self.net.load_state_dict(torch.load(self.model, map_location=self.device))
         self.net.eval()
         return self.device, self.net
 
-    def show(self):
-        """Shows if given image has a badminton court"""
+    # def show(self):
+    #     """Shows if given image has a badminton court"""
 
-        img = cv2.imread(self.model, cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (img_wid, img_hei))
-        img_arr = np.array(img)
-        X = torch.Tensor(img_arr).view(-1, 1, img_wid, img_hei)
-        X = X/255.0
-        with torch.no_grad():
-            X = X.to(self.device)
-            output = net(X)
-            output = torch.argmax(output)
-            if output == 0:
-                print(f'{self.model} Random')
-            elif output == 1:
-                print(f'{self.model} Badminton Court')
-                cv2.imshow(self.model, img)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+    #     img = cv2.imread(self.model, cv2.IMREAD_GRAYSCALE)
+    #     img = cv2.resize(img, (img_wid, img_hei))
+    #     img_arr = np.array(img)
+    #     X = torch.Tensor(img_arr).view(-1, 1, img_wid, img_hei)
+    #     X = X/255.0
+    #     with torch.no_grad():
+    #         X = X.to(self.device)
+    #         output = net(X)
+    #         output = torch.argmax(output)
+    #         if output == 0:
+    #             print(f'{self.model} Random')
+    #         elif output == 1:
+    #             print(f'{self.model} Badminton Court')
+    #             cv2.imshow(self.model, img)
+    #             cv2.waitKey(0)
+    #             cv2.destroyAllWindows()
 
         
 inf = Inference('bcf_models/model-1608419488-lr1e-06-factor0.1pat2-thr0.01-val_pct0.2-bat_size150-128-72-512.pth', "cuda:0")
 
 
-directory = "bcf_images/random/"
+# directory = "bcf_images/random/"
 # directory = "bcf_images/badminton_court"
 # directory = "bcf_images/overflow"
 
