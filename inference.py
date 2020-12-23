ac6a00e81a8fdf2266c1c14423ac2accc45fed5e
mspencersmith <mspencersmith@outlook.com>
2020-12-23 09:30:16 +0000

    Updated Inference

--- inference.py
+++ inference.py
@@ -7,28 +7,33 @@ from nn_model import Net
 
 start = time.time()
 
+img_wid = 128
+img_hei = 72
+
 def load_model(file):
     """Loads model on to GPU if availible otherwise loads to CPU"""
     
     if torch.cuda.is_available():
         print(f"\nLoading neural network on GPU..\n")
-        device = torch.device("cuda:0")
-        net = Net().to(device)
-        net.load_state_dict(torch.load(file, map_location=device))
-        net.eval()
+        device, net = to_dev(file, "cuda:0")
     else:
         print(f"\nLoading neural network on CPU..\n")
-        device = torch.device("cpu")
-        net = Net().to(device)
-        net.load_state_dict(torch.load(file, map_location=device))
-        net.eval()
+        device, net = to_dev(file, "cpu")
+
+    return device, net
+
+def to_dev(file, processor):
+    """Transfers model to device"""
+    
+    device = torch.device(processor)
+    net = Net().to(device)
+    net.load_state_dict(torch.load(file, map_location=device))
+    net.eval()
     return device, net
 
 def show(file, device, net):
-    """Detects if given image has a badminton court"""
+    """Shows if given image has a badminton court"""
 
-    img_wid = 128
-    img_hei = 72
     img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (img_wid, img_hei))
     img_arr = np.array(img)
@@ -38,31 +43,41 @@ def show(file, device, net):
         X = X.to(device)
         output = net(X)
         output = torch.argmax(output)
-        return output
+        if output == 0:
+            print(f'{file} Random')
+        elif output == 1:
+            print(f'{file} Badminton Court')
+            cv2.imshow(file, img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-device, net = load_model('bcf_models/model-1608175256-lr1e-06-factor0.1pat2-thr0.01-val_pct0.2-bat_size150-128-72-512.pth')
+        
+
+device, net = load_model('bcf_models/model-1608419488-lr1e-06-factor0.1pat2-thr0.01-val_pct0.2-bat_size150-128-72-512.pth')
 
 finish = time.time()
 mins = round((finish - start)/60, 2)
 print(f"\nTotal time taken {mins} minutes.")
 
-directory = "badminton_court_finder/bcf_images/random"
-# directory = "badminton_court_finder/bcf_images/badminton_court"
-# directory = "badminton_court_finder/bcf_images/overflow"
+directory = "bcf_images/random/"
+# directory = "bcf_images/badminton_court"
+# directory = "bcf_images/overflow"
 
-# show("badminton_court_finder/bcf_images/overflow/0.jpg", device, net)
+# show("bcf_images/random/0.jpg", device, net)
 
 # for f in os.listdir(directory):
 #     path = os.path.join(directory, f)
 #     show(path, device, net)
 
-for f in range(10848, 11000):
+for f in range(0, 100):
+
+    try:
+        path = (f'{directory}{f}.jpeg')
+        show(path, device, net)
+    except cv2.error as e2:
+        pass
     try:
         path = (f'{directory}{f}.jpg')
         show(path, device, net)
     except cv2.error as e:
-        raise e
-    else:
-        path = (f'{directory}{f}.jpeg')
-        show(path, device, net)
-
+        pass
